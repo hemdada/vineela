ddbb765cff86e2103b199fb3c03fddda8cf1d8a1
Hemanth Reddy <hemanth.sannareddy@wavemaker.com>
2016-11-21 15:04:37 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "vineela",
+  "homePage" : "Main",
+  "name" : "vineela",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
